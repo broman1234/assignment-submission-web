121cbbbe6b3c798c5fc64361db9a72def01f0467
manmo <man.mo@thoughtworks.com>
2023-04-11 12:03:39 +0800

    adding ReactJs front end to the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    console.log("Hello!");
+
+    const reqBody = {
+        "username": "manmo",
+        "password": "abc"
+    }
+
+    fetch("api/auth/login", {
+        headers: {
+            "Content-Type": "application/json",
+        },
+        method: "post",
+        body: JSON.stringify(reqBody)
+    }).then(response => Promise.all([response.json(), response.headers]))
+        .then(([body, headers]) => {
+            const authValue = headers.get("authorization");
+            console.log(authValue);
+            console.log(body);
+        });
+
+    return (
+        <div className="App">
+            <h1>Hello World!</h1>
+        </div>
+    );
 }
 
 export default App;
